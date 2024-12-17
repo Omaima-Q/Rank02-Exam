6497ce79320e4df162c0c3cafcc9417f447780e8
Omaima-Q <oqaroot@student.42abudhabi.ae>
2024-12-17 18:28:47 +0400

    Update is_power_of_2.c

--- Level 2/is_power_of_2/is_power_of_2.c
+++ Level 2/is_power_of_2/is_power_of_2.c
@@ -1,6 +1,27 @@
-int is_power_of_2(unsigned int n)
+int is_power_of_2(unsigned int n) //you can either use this way 
 {
     if (n == 0)
         return 0;
     return (n & (n - 1)) == 0;
 }
+
+//or
+
+int is_power_of_2(unsigned int n) //this way
+{
+    return (n && !(n & (n - 1)));
+}
+
+// example
+// if n = 16
+// n - 1 = 15
+// use bitwise operator & 
+// 10000
+// 01111
+// ------
+// 00000
+
+// it means that 16 is power of 2
+
+// if n = 0
+// just return 0
