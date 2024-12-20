cbad5630a915058ad618c9a8b5461d125cb7bf52
Omaima-Q <oqaroot@student.42abudhabi.ae>
2024-12-20 21:55:18 +0400

    Update test.c

--- Level 2/print_bits/test.c
+++ Level 2/print_bits/test.c
@@ -1,1 +1,32 @@
+#include <unistd.h>
+
+void print_bits(unsigned char octet)
+{
+	int i = 8;
+	unsigned char 	bit;
+
+	while (i--)
+	{
+		bit = (octet >> i & 1) + '0';
+		write(1, &bit, 1);
+	}
+}
+
+int main(void) 
+{
+    unsigned char value;
+
+    // Example: Let's use the value 5 (which is 00000101 in binary)
+    value = 50;
+
+    // Call the print_bits function to print the binary representation
+    print_bits(value);
+
+    // Print a newline for better output formatting
+    write(1, "\n", 1);
+
+    return 0;
+}
+
+// output is : 00110010 for the value 50
 
