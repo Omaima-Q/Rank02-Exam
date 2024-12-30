80ee1b63c4d73b41e89885316eac04aeccefe3fb
Omaima-Q <oqaroot@student.42abudhabi.ae>
2024-12-30 16:32:58 +0400

    Update ft_strcpy.c

--- Level 1/ft_strcpy/ft_strcpy.c
+++ Level 1/ft_strcpy/ft_strcpy.c
@@ -10,3 +10,12 @@ char	*ft_strcpy(char *s1, char *s2)
 	s1[i] = '\0';
 	return (s1);
 }
+
+// int	main(void)
+// {
+// 	char str1[] = "omaima";
+// 	char str2[] = "good";
+// 	ft_strcpy(str1, str2);
+// 	printf("%s \n%s", str1, str2);
+// 	return (0);
+// }
