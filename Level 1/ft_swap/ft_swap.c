7bf3ff9301df83abc26f7f0615103a99e234f68c
Omaima-Q <oqaroot@student.42abudhabi.ae>
2024-12-30 16:36:42 +0400

    Update ft_swap.c

--- Level 1/ft_swap/ft_swap.c
+++ Level 1/ft_swap/ft_swap.c
@@ -4,3 +4,14 @@ void	ft_swap(int *a, int *b)
 	*a = *b;
 	*b = temp;
 }
+
+// int	main(void)
+// {
+// 	int x = 5;
+// 	int y = 9;
+
+// 	printf("numbers before swap %d and %d \n", x, y);
+// 	ft_swap(&x, &y);
+// 	printf("numbers after swap %d and %d", x, y);
+// 	return (0);
+// }
