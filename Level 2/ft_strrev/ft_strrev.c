13cab4e849d02c3645f81416c8ab58151cbea612
Omaima-Q <oqaroot@student.42abudhabi.ae>
2024-12-30 20:55:12 +0400

    Update ft_strrev.c

--- Level 2/ft_strrev/ft_strrev.c
+++ Level 2/ft_strrev/ft_strrev.c
@@ -1,16 +1,19 @@
 char *ft_strrev(char *str)
 {
-	int i = -1;
-	int length = 0;
-	char temporary;
+    int i = -1; // Initialize index i to -1
+    int length = 0; // Initialize length to 0
+    char temporary; // Temporary variable for swapping characters
 
-	while (str[length])
-		length++;
-	while (++i < length / 2)
-	{
-		temporary = str[i];
-		str[i] = str[length - 1 - i];
-		str[length - 1 - i] = temporary;
-	}
-	return (str);
+    // Calculate the length of the string
+    while (str[length]) // Loop until we reach the null terminator
+        length++; // Increment length for each character
+
+    // Reverse the string in place
+    while (++i < length / 2) // Loop until the middle of the string
+    {
+        temporary = str[i]; // Store the current character in temporary
+        str[i] = str[length - 1 - i]; // Swap the characters
+        str[length - 1 - i] = temporary; // Complete the swap
+    }
+    return (str); // Return the reversed string
 }
