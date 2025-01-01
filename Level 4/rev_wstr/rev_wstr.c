89b69642fa560ea62ec8ce5c884c9d0c7b63bca3
Omaima-Q <oqaroot@student.42abudhabi.ae>
2025-01-02 01:28:51 +0400

    Update rev_wstr.c

--- Level 4/rev_wstr/rev_wstr.c
+++ Level 4/rev_wstr/rev_wstr.c
@@ -8,13 +8,13 @@ int	main(int ac, char **av)
 
 	if (ac == 2)
 	{
-		while (av[1][i] != '\0')
+		while (av[1][i] != '\0')// iterate until the end of the string
 			i++;
-		while (i >= 0)
+		while (i >= 0) // keep iterating until the begonning of the string 
 		{
 			while (av[1][i] == '\0' || av[1][i] == ' ' || av[1][i] == '\t')
-				i--;
-			end = i;
+				i--; // iterate backwards until it finds null or space or tab
+			end = i; // i now at the end of the word
 			while (av[1][i] != '\0' && av[1][i] != ' ' && av[1][i] != '\t')
 				i--;
 			start = i + 1;
