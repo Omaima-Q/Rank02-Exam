119cbdca75457f294296acce6ce296a4a02958de
Omaima-Q <oqaroot@student.42abudhabi.ae>
2025-01-01 17:05:57 +0400

    Update paramsum.c

--- Level 3/paramsum/paramsum.c
+++ Level 3/paramsum/paramsum.c
@@ -1,20 +1,22 @@
 #include <unistd.h>
 
-void	ft_putnbr(int n)
+void	ft_putnbr(int n) // use the putnbr function to print the numbers
 {
 	char digit;
 
 	if (n >= 10)
 		ft_putnbr(n / 10);
-	digit = (n % 10) + '0';
+	digit = (n % 10) + '0'; // // '0' is added to convert the integer to its ASCII character
 	write(1, &digit, 1);
 }
 
 int	main(int argc, char **argv)
 {
-	(void)argv;
+	(void)argv; // Ignore the argv parameter as it's not used
 
-	ft_putnbr(argc - 1);
+	ft_putnbr(argc - 1); // the reason for using (argc - 1)
+	//is to count the number of command-line arguments passed 
+	// to the program, excluding the program's name itself.
 	write(1, "\n", 1);
 	return (0);
 }
