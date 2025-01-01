79bb794b39ca4cc481027940962b229c7ea1f921
Omaima-Q <oqaroot@student.42abudhabi.ae>
2025-01-01 17:11:05 +0400

    Update paramsum.c

--- Level 3/paramsum/paramsum.c
+++ Level 3/paramsum/paramsum.c
@@ -10,6 +10,15 @@ void	ft_putnbr(int n) // use the putnbr function to print the numbers
 	write(1, &digit, 1);
 }
 
+//or we can use this putnbr function
+
+void	ft_putnbr(int n)
+{
+	if (n > 9)
+		ft_putnbr(n / 10);
+	write(1, &"0123456789"[n % 10], 1);
+}
+
 int	main(int argc, char **argv)
 {
 	(void)argv; // Ignore the argv parameter as it's not used
