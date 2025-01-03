17f3ef862362b3373d6610162ce5763d81762ad5
Omaima-Q <oqaroot@student.42abudhabi.ae>
2025-01-03 21:38:00 +0400

    Update ft_atoi_base.c

--- Level 3/ft_atoi_base/ft_atoi_base.c
+++ Level 3/ft_atoi_base/ft_atoi_base.c
@@ -1,33 +1,44 @@
-#define abs(x) x < 0 ? -x : x
+#include <stdio.h>
+#include <stdlib.h>
 
-int is_correct(char c) // check if the char is vallid
+int ft_atoi_base(const char *str, int str_base)
 {
-	return ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f'));
+    int i = 0;
+    int result = 0;
+    int sign = 1;
+
+    while (str[i] && ((str[i] >= 9 && str[i] <= 13) || str[i] == 32))
+        i++;
+
+    if (str[i] == '-')
+        sign = -sign;
+
+    if (str[i] == '+' || str[i] == '-')
+        i++;
+
+    while (str[i])
+    {
+        int digit_value = -1;
+
+        if (str[i] >= '0' && str[i] <= '9')
+            digit_value = str[i] - '0';
+        else if (str_base <= 16 && str[i] >= 'a' && str[i] <= 'f')
+            digit_value = 10 + str[i] - 'a';
+        else if (str_base <= 16 && str[i] >= 'A' && str[i] <= 'F')
+            digit_value = 10 + str[i] - 'A';
+
+        if (digit_value < 0 || digit_value >= str_base)
+            break;
+
+        result = result * str_base + digit_value;
+        i++;
+    }
+
+    return (result * sign);
 }
 
-int		ft_atoi_base(const char *str, int str_base)
+/*int main (int ac, char **av)
 {
-	int i = 0;
-	int sign = 1;
-	unsigned int nb = 0;
-	
-	while (str[i] == ' ') // check if there is a space 
-		i++;
-	if (str[i] == '-' || str[i] == '+') // if it's negative or positive 
-	{
-		if (str[i] == '-')
-			sign = -1;
-		i++;
-	}
-	while (is_correct(str[i]) && (str_base <= 16) && str[i] != 0) // valid base and correct char 
-	{
-		if(str[i] >= '0' && str[i] <= '9')
-			nb = nb *str_base + str[i] - '0';
-		if ((str[i] >= 'A' && str[i] <= 'F') && (abs(str_base) >= 11)) // when its a bigger case
-			nb = nb *str_base + str[i] - 'A' + 10;
-		if ((str[i] >= 'a' && str[i] <= 'z') && (abs(str_base) >= 11)) // when its a smaller case
-			nb = nb *str_base + str[i] - 'a' + 10;
-		i++;
-	}
-	return (nb * sign);
-}
+	(void)ac;
+	printf("%d\n", ft_atoi_base(av[1], atoi(av[2])));
+}*/
