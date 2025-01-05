566ce08566d737a003f48ead1f0cf88a03442c35
Omaima-Q <oqaroot@student.42abudhabi.ae>
2025-01-05 18:12:33 +0400

    Update rostring.c

--- Level 4/rostring/rostring.c
+++ Level 4/rostring/rostring.c
@@ -27,7 +27,7 @@ int main (int ac , char **av)
 				      {
 					          while (av[1][i] && (av[1][i] != ' ' && av[1][i] != '\t'))
 					          {
-                          write (1, &av[1][i], 1);
+                          					write (1, &av[1][i], 1);
 						              i++;
 					          }
 					          write (1, " ", 1);
