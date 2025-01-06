45ff376a187a62c5cfa3504c6a6cc1d0670695ae
Omaima-Q <oqaroot@student.42abudhabi.ae>
2025-01-06 18:59:18 +0400

    Update ft_strcspn.c

--- Level 2/ft_strcspn/ft_strcspn.c
+++ Level 2/ft_strcspn/ft_strcspn.c
@@ -8,16 +8,18 @@ size_t ft_strcspn(const char *s, const char *reject)
     
     i = 0;
     // Looping over the whole string
-    while(s[i++])
+    while(s[i])
     {
         j = 0;
         // Check the current string char against every char in charset
         while (reject[j])
         {
             // If character found, return index in current string
-            if (s[i] == reject[j++])
+            if (s[i] == reject[j])
             	return (i);
+            j++;
         }
+        i++;
     }
     // If we spanned the whole string, return i, in that case the
     // rejected char found is the NULL-terminating one
