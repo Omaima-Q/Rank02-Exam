e039051a953f56c847cf5f48f47fa58c44a6db37
Omaima-Q <oqaroot@student.42abudhabi.ae>
2025-01-06 21:59:35 +0400

    Update last_word.c

--- Level 2/last_word/last_word.c
+++ Level 2/last_word/last_word.c
@@ -2,22 +2,22 @@
 
 int main(int ac, char **av)
 {
-	int i = 0;
-
-	if (ac == 2)
-	{
-		while(av[1][i])
-			i++; // iteration util the end of the string
-		i--; //iterate backwards after the null terminator (last character of the last word) 
-		while(av[1][i] > 32)
-			i--; // iterate backwards until we find a space 
-		i++; // iterate forward the last string (which is the last word)
-		while(av[1][i])
-		{
-			write(1, &av[1][i], 1); // print the last word
-			i++;
-		}
-	}
-	write(1, "\n", 1);
-	return (0);
+    if (ac == 2)
+    {
+        int i = 0;
+        while (av[1][i])
+            i++;
+        i--;
+        while (av[1][i] && (av[1][i] == ' ' || av[1][i] == '\t'))
+            i--;
+        while (av[1][i] && (av[1][i] != ' ' && av[1][i] != '\t'))
+            i--;
+        i++;
+        while (av[1][i] && (av[1][i] != ' ' && av[1][i] != '\t'))
+        {
+            write (1, &av[1][i], 1);
+            i++;
+        }
+    }
+    write(1, "\n", 1);
 }
