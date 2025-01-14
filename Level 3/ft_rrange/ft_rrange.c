7380e3e3f6d726869014dd163af4367563d30966
Omaima-Q <oqaroot@student.42abudhabi.ae>
2025-01-14 18:24:46 +0400

    Update ft_rrange.c

--- Level 3/ft_rrange/ft_rrange.c
+++ Level 3/ft_rrange/ft_rrange.c
@@ -1,27 +1,43 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-int *ft_rrange(int start, int end)
+int *ft_range(int start, int end)
 {
-	int *range;
-	int i = 0;
-	int step = 1;
-	int n = end - start;
-
-	if (n < 0)
-		(n *= -1);
-	n++;
-
-	range = (int *)malloc(sizeof(int) * n);
-	if (range)
-	{
-		if (start < end)
-			step = -1;
-		while (i < n)
-		{
-			range[i] = end;
-			end = end + step;
-			i++;
-		}
-	}
-	return (range);
+    int i = 0;
+    int *result;
+    int len;
+    if (end > start)
+        len = end - start + 1;
+    else
+        len = start - end + 1;
+    result = malloc(len * sizeof(int));
+    if (!result)
+        return(NULL);
+    while (len > i)
+    {
+        if (end > start)
+        {
+            result[i] = end;
+            end--;
+        }
+        else
+        {
+            result[i] = end;
+            end++;
+        }
+        i++;
+    }
+    return(result);
 }
+
+// int main(void)
+// {
+//     int size = 4;
+//     int *result = ft_range(0, -3);
+//     int i = 0;
+//     while (size > i)
+//     {
+//         printf("%d\n", result[i]);
+//         i++;
+//     }
+// }
