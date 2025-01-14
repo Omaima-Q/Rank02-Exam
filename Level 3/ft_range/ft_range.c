22273539c725ef7e3a1be03ba73a60dfaf62ee8c
Omaima-Q <oqaroot@student.42abudhabi.ae>
2025-01-14 18:27:39 +0400

    Update ft_range.c

--- Level 3/ft_range/ft_range.c
+++ Level 3/ft_range/ft_range.c
@@ -23,3 +23,50 @@ int *ft_range(int start, int end)
 	}
         return (res);
 }
+
+or 
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int *ft_range(int start, int end)
+{
+    int i = 0;
+    int *result;
+    int len;
+    if (end > start)
+        len = end - start + 1;
+    else
+        len = start - end + 1;
+    result = malloc(len * sizeof(int));
+    if (!result)
+        return(NULL);
+    while (len > i)
+    {
+        if (end > start)
+        {
+            result[i] = start;
+            start++;
+        }
+        else
+        {
+            result[i] = start;
+            start--;
+        }
+        i++;
+    }
+    return(result);
+}
+
+// int main(void)
+// {
+//     int size = 4;
+//     int *result = ft_range(0, -3);
+//     int i = 0;
+//     while (size > i)
+//     {
+//         printf("%d\n", result[i]);
+//         i++;
+//     }
+// }
