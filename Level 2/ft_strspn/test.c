e530477a0d52758ae010666f71dcc81fe3d6af98
Omaima-Q <oqaroot@student.42abudhabi.ae>
2025-01-25 21:14:01 +0400

    Create test.c

---
+++ Level 2/ft_strspn/test.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <stdio.h>
+
+int  is_char_in_string(char c, const char *str)
+{
+  while (*str)
+    if (*str++ == c)
+      return (1);
+  return (0);
+}
+
+size_t  ft_strspn(const char *s, const char *accept)
+{
+  size_t  count = 0;
+
+  while (*s)
+  {
+    if (is_char_in_string(*s, accept))
+      count++;
+    else
+      break ;
+    s++;
+  }
+  return (count);
+}
