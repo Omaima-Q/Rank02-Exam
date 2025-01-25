a41db4554dae5c834d06118083fce15f8863932b
Omaima-Q <oqaroot@student.42abudhabi.ae>
2025-01-25 21:06:25 +0400

    Create test2.c

---
+++ Level 3/ft_range/test2.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+
+int     *ft_range(int start, int end)
+{
+  int  *res;
+  int  size;
+  int  i = -1;
+
+  if (start < end)
+  {
+    size = end - start + 1;
+    res = malloc(sizeof(int) * size);
+  }
+  else if (start > end)
+  {
+    size = start - end + 1;
+    res = malloc(sizeof(int) * size);
+  }
+  else
+  {
+    size = start - end + 1;
+    res = malloc(sizeof(int) * size);
+  }
+  if (res)
+    while (++i < size)
+      res[i] = start++;
+  return (res);
+}
+
+static int  ft_abs(int x)
+{
+  if (x < 0)
+    return (-x);
+  return (x);
+}
+
+#include <stdio.h>
+
+int  main(int ac, char **av)
+{
+  ac += 0;
+  int  a = atoi(av[1]);
+  int  b = atoi(av[2]);
+  int  s = ft_abs(a - b) + 1;
+  int *result = ft_range(a, b);
+  int  i = -1;
+  
+  while (++i < s)
+    printf("%d ", result[i]);
+  printf("\n");
+}
