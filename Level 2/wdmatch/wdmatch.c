403f81dde8c9f9d99fdde80ca432996e63c42869
Omaima-Q <oqaroot@student.42abudhabi.ae>
2025-01-25 20:54:00 +0400

    Update wdmatch.c

--- Level 2/wdmatch/wdmatch.c
+++ Level 2/wdmatch/wdmatch.c
@@ -16,14 +16,8 @@ int main(int ac, char **av)
             j++;
         }
         if (av[1][i] == '\0')
-        {
-            i = 0;
-            while(av[1][i])
-            {
-                write(1, &av[1][i], 1);
-                i++;
-            }
-        }
+            write(1, av[1], i);
+
     }
     write(1, "\n", 1);
     return(0);
