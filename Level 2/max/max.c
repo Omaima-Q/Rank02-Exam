e5391f117c751e035829740eb4e245da5716bf33
Omaima-Q <oqaroot@student.42abudhabi.ae>
2025-01-26 15:31:35 +0400

    Update max.c

--- Level 2/max/max.c
+++ Level 2/max/max.c
@@ -14,3 +14,18 @@ int max(int *tab, unsigned int len)
 	}
 	return result;
 }
+
+// #include <stdio.h>
+//
+// int	main(void)
+// {
+// 	int tab[] = {24, 67, 89, 78};                   
+// 		// Initialize an array of integers
+// 	unsigned int len = sizeof(tab) / sizeof(tab[0]);
+// 		// Calculate the length of the array
+// 	int result = max(tab, len);                     
+// 		// Call max with the array and its length
+// 	printf("%d\n", result);                          // Print the result
+// 	return (0);
+// }
+
