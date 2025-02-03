72c9a95f85f2a4a9284a62af40b4520e1010e379
Omaima-Q <oqaroot@student.42abudhabi.ae>
2025-02-03 18:57:39 +0400

    Update print_bits.c

--- Level 2/print_bits/print_bits.c
+++ Level 2/print_bits/print_bits.c
@@ -1,9 +1,14 @@
+// passed in rank 02 exam
+
 #include <unistd.h>
 
 void print_bits(unsigned char octet)
 {
 	int i = 8;
 	unsigned char 	bit;
+
+	if (bit == 0 || octet == 0)
+		return;
 
 	while (i--)
 	{
