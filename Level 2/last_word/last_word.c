c693382264e5d731a1fb38ef1b217b147d717307
Omaima-Q <oqaroot@student.42abudhabi.ae>
2025-02-03 18:45:39 +0400

    Update last_word.c

--- Level 2/last_word/last_word.c
+++ Level 2/last_word/last_word.c
@@ -7,13 +7,13 @@ int main(int ac, char **av)
         int i = 0;
         while (av[1][i])
             i++;
-        i--;
+        i--; // you are at the end of the last word here, after skipping the null terminator
         while (av[1][i] && (av[1][i] == ' ' || av[1][i] == '\t'))
-            i--;
+            i--; // skip spaces and tabs 
         while (av[1][i] && (av[1][i] != ' ' && av[1][i] != '\t'))
             i--;
-        i++;
-        while (av[1][i] && (av[1][i] != ' ' && av[1][i] != '\t'))
+        i++; // you are at the beginning of the last word here 
+        while (av[1][i] && (av[1][i] != ' ' && av[1][i] != '\t')) // now you can print the last word 
         {
             write (1, &av[1][i], 1);
             i++;
