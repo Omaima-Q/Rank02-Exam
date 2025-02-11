b0645c27cb14c32d8732fd9dce559f0df8e88503
Omaima-Q <oqaroot@student.42abudhabi.ae>
2025-02-11 15:59:14 +0400

    Update rotone.c

--- Level 1/rotone/rotone.c
+++ Level 1/rotone/rotone.c
@@ -1,28 +1,20 @@
+// passed in the exam
+
 #include <unistd.h>
 
-void	ft_putchar(char c)
+int main(int ac, char **av)
 {
-	write(1, &c, 1);
-}
-
-void	rotone(char *s)
-{
-	while (*s)
-	{
-		if ((*s >= 'A' && *s <= 'Y') || (*s >= 'a' && *s <= 'y'))
-			ft_putchar(*s + 1);
-		else if (*s == 'Z' || *s == 'z')
-			ft_putchar(*s - 25);
-		else
-			ft_putchar(*s);
-		++s;
-	}
-}
-
-int		main(int ac, char **av)
-{
-	if (ac == 2)
-		rotone(av[1]);
-	ft_putchar('\n');
-	return (0);
+    int i = 0;
+    if (ac == 2)
+    {
+        while (av[1][i])
+        {
+            if ((av[1][i] >= 'A' && av[1][i] <= 'Y') || (av[1][i] >= 'a' && av[1][i] <= 'y'))
+                av[1][i] += 1;
+            else if ((av[1][i] == 'z' || av[1][i] == 'Z'))
+                av[1][i] -= 25;
+            write(1, &av[1][i], 1);
+            i++;
+        }
+    } 
 }
